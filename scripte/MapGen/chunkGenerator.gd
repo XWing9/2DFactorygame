3df55e831fa6b9017f793640b236ca8314921184
XWing <henri.haege@icloud.com>
2026-01-21 23:01:35 +0100

    new better? gen method

--- scripte/MapGen/chunkGenerator.gd
+++ scripte/MapGen/chunkGenerator.gd
@@ -17,10 +17,10 @@ var chunk_origin_y
 signal extended_ChunkGen_Finished
 var offset : int = int(-0.5 * chunk_Size)
 var dic_Key : Vector2i
-var dirtCords : Array[Vector2i] = []
-var grassCords : Array[Vector2i] = []
+var tmp_Tile_Pos_Array : Array
 var tmp_Tile_Pos : Vector2i = Vector2i.ZERO
 var tmpVectorCords : Vector2i
+var tmpVectorCordsArray : Array
 var startingY : int
 var startingX : int
 var tileXPos : int
@@ -34,18 +34,20 @@ var tempChunkData : Dictionary = {
 }
 
 var genRuleBookDic : Dictionary = {
-	"GrassLands" : {
-		"Grass": Vector2i(2,1),
-		"Dirt": Vector2i (7,1),
-		"IronOre": Vector2i(12,15)
-	},
-	"Forest" : {
-		"ForestGrass" : Vector2i(2,3)
-	}
+	"GrassLands" : [
+		Vector2i(12,15), #Iron
+		Vector2i (7,1), #Dirt
+		Vector2i(2,1), #Grass
+		Vector2i(2,1) #Grass
+	], #change rest
+	"Forest" : [
+		Vector2i(2,3), #ForestGrass
+		Vector2i(4,5), #whatever
+	]
 }
 
-var biome := "GrassLands" # later: get_biome_from_noise(chunk_Cords_X, chunk_Cords_Y)
-var biome_tiles : Dictionary = genRuleBookDic[biome]
+var biome : String
+var biome_tiles : Array
 var tmpAtlasCords : Vector2i
 
 var grassAtlas : Vector2i
@@ -66,6 +68,10 @@ func generateChunks(noise,tilemap):
 			chunk_origin_y = (chunk_y - 1.5) * chunk_Size
 			chunk_Cords_X = chunk_x - (startingchunks >> 1)
 			chunk_Cords_Y = chunk_y - (startingchunks >> 1)
+			
+			biome = determineBiome(chunk_Cords_X,chunk_Cords_Y)
+			biome_tiles = genRuleBookDic[biome]
+			
 			#loop to generate chunk tiles
 			for x in range(chunk_Size):
 				for y in range(chunk_Size):
@@ -73,13 +79,8 @@ func generateChunks(noise,tilemap):
 					tileYPos = chunk_origin_y + y
 					noise_val = noise.get_noise_2d(tileXPos, tileYPos)
 
-					if noise_val >= 0.5:
-						tmpAtlasCords = biome_tiles["IronOre"]
-					elif noise_val >= 0.0:
-						tmpAtlasCords = biome_tiles["Dirt"]
-					else:
-						tmpAtlasCords = biome_tiles["Grass"]
-						
+					tmpAtlasCords = determineTile(noise_val,biome_tiles)
+					
 					tilemap.set_cell(Vector2i(tileXPos, tileYPos), sourceId, tmpAtlasCords)
 					tempdic["tilepos"].append(Vector2i(tileXPos,tileYPos))
 			
@@ -91,11 +92,16 @@ func generateChunks(noise,tilemap):
 func extendedChunkGen(toGenChunks, noise,tilemap):
 	for chunks in range (toGenChunks.size()):
 		tempdic = dupdic()
-		dirtCords.clear()
-		grassCords.clear()
+		tmpVectorCordsArray.clear()
+		tmp_Tile_Pos_Array.clear()
 		tmpVectorCords = toGenChunks[chunks]
 		startingX = chunk_Size * int(tmpVectorCords.x)
 		startingY = chunk_Size * int(tmpVectorCords.y)
+		
+		#determine biome
+		biome = determineBiome(startingX,startingY)
+		biome_tiles = genRuleBookDic[biome]
+		
 		#loop to generate chunk tiles
 		for chunk_X in range(chunk_Size):
 			for chunk_Y in range(chunk_Size):
@@ -103,17 +109,15 @@ func extendedChunkGen(toGenChunks, noise,tilemap):
 				tileYPos = startingY + chunk_Y + offset
 				tmp_Tile_Pos = Vector2i(tileXPos,tileYPos)
 				noise_val = noise.get_noise_2d(tileXPos, tileYPos)
+
+				tmpAtlasCords = determineTile(noise_val,biome_tiles)
+				tmpVectorCordsArray.append(tmpAtlasCords)
+				tmp_Tile_Pos_Array.append(tmp_Tile_Pos)
 				
-				if noise_val >= 0.0:
-					dirtCords.append(tmp_Tile_Pos)
-				else:
-					grassCords.append(tmp_Tile_Pos)
 				tempdic["tilepos"].append(Vector2i(tileXPos,tileYPos))
 		#batch set tiles
-		for pos in dirtCords:
-			tilemap.set_cell(pos, sourceId, dirtAtlas)
-		for pos in grassCords:
-			tilemap.set_cell(pos, sourceId, grassAtlas)
+		for i in range(tmp_Tile_Pos_Array.size()):
+			tilemap.set_cell(tmp_Tile_Pos_Array[i], sourceId, tmpVectorCordsArray[i])
 		
 		dic_Key = Vector2i(tmpVectorCords.x,tmpVectorCords.y)
 		chunk_Data.Loaded_Chunks[dic_Key] = tempdic
@@ -125,8 +129,22 @@ func emit_Finished_Signal():
 func dupdic() -> Dictionary:
 	return {"tilepos": []}.duplicate(true)
 
-func determineTile():
-	#use function to send noise and biom to to determine tile
-	pass
-#make a rulebook for what tile is for what noise
+func determineTile(noise, listOfTiles):
+	print(noise)
+	#i have no ide what im doing here?
+	
+	var t = clamp((noise + 1.0) * 0.5, 0.0, 0.999)
+	var index := int(t * listOfTiles.size())
+	
+	return listOfTiles[index]
+
+
+func determineBiome(x: int, y: int):
+	return "GrassLands"
 #consider multithreading
+
+#next gen steps:
+#Separate biome noise from terrain noise
+#Ore clusters using a second noise
+#Biome blending at chunk borders
+#Weighted ores per biome
